e70245948b448b074089175c92ba79574c166bf7
Inforlozzi-ai <alvarolozziferrari@gmail.com>
2026-04-22 20:17:35 -0300

    feat: register BotsInternalController in BotsModule

--- apps/api/src/bots/bots.module.ts
+++ apps/api/src/bots/bots.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Bot } from './bot.entity';
 import { BotsService } from './bots.service';
-import { BotsController } from './bots.controller';
+import { BotsController, BotsInternalController } from './bots.controller';
 import { AuthModule } from '../auth/auth.module';
 import { ProvisionerModule } from '../provisioner/provisioner.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Bot]), AuthModule, ProvisionerModule],
   providers: [BotsService],
-  controllers: [BotsController],
+  controllers: [BotsController, BotsInternalController],
   exports: [BotsService],
 })
 export class BotsModule {}
